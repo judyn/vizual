733a549a73a70fc88bd700b384cd392ad61cbc7b
Judy <judynguyen90@gmail.com>
2016-03-20 15:46:34 -0500

    waveform function

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
 
 	colorMode(HSB, 100);
-	 frameRate(30);
+    frameRate(30);
 	analyzer = new p5.Amplitude();
 	analyzer.setInput(song);
 	fft = new p5.FFT();
@@ -34,20 +34,34 @@ function mousePressed() {
   
   } else {
  
-  
 		textAlign(CENTER);
 		text("Pause",width/2, height/2);
-    song.play(); // playback will resume from the pause position
+        song.play(); // playback will resume from the pause position
   }
   
 ellipse(mouseX,mouseY,20+rms*400, 20+rms*400);
 
 }
 
+function waveForm(){
+  
+     var spectrum = fft.analyze(); 
+     var rms = analyzer.getLevel();
+    
+    for (var i = 0; i < spectrum.length/20; i++) {
+    fill(spectrum[rms]/2, spectrum[rms]/10, 0);
+    var x = map(i, 0, spectrum.length/25, 0, width);
+    var h = map(spectrum[i], 0, 255, 0, height/2);
+    rect(x, height, spectrum.length/25, -h);
+   
+    }
+    fill(spectrum);
+    
+}
 
 function draw() {
   background(255);
-  var spectrum = fft.analyze(); 
+ 
     v1++;
   if(v1 > 100){
   v1 = 0;
@@ -70,8 +84,6 @@ function draw() {
   ellipse(mouseX,mouseY,20+rms*400, 20+rms*400);
   noStroke();
     
-
-  
    /*for (var i = 0; i< spectrum.length; i++){
     var x = map(i, 0, spectrum.length, 0, width);
     var h = -height + map(spectrum[i], 0, width+5, height, 0);
@@ -79,16 +91,11 @@ function draw() {
     rect(x, height/2, width/2 / spectrum.length, h );
    
   }*/
-    for (var i = 0; i < spectrum.length/20; i++) {
-    fill(spectrum[rms]/2, spectrum[rms]/10, 0);
-    var x = map(i, 0, spectrum.length/25, 0, width);
-    var h = map(spectrum[i], 0, 255, 0, height/2);
-    rect(x, height, spectrum.length/25, -h);
-   
-    }
-      fill(spectrum);
+  
+    waveForm();
   
 }
+
 
 
 
